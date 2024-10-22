c7c3a704488ae9dc4da24ff5d1c3151d148f0967
Zubin Darbari <zmd911@gmail.com>
2024-10-22 11:24:15 -0400

    first commit

---
+++ apps.js
@@ -0,0 +1,8 @@
+const amount = 19
+
+if (amount < 10) {
+    console.log(`small number`)
+} else {
+    console.log(`large number`)
+}
+console.log('1st node app')
---
+++ logger.js
@@ -0,0 +1,8 @@
+
+var url = 'http://af.io/log' ;
+
+function log(message) {
+    console.log(message);
+}
+
+module.exports.log = log;
